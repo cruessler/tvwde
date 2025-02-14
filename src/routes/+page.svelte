f97d37cb58185f51a93945200c43b1efb91974c2
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-02-14 14:24:36 +0100

    Update /
    - Replace `description_de` by `subtitle_de`
    - Reduce most margins
    - Reduce most font sizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,9 +14,9 @@
   const { year, month } = getCurrentYearAndMonth();
 </script>
 
-<div class="relative mb-8 h-80 w-full bg-slate-200">
+<div class="relative mb-6 h-80 w-full bg-slate-200">
   <div
-    class="absolute bottom-15 left-0 line-clamp-2 w-86 bg-red-900/85 px-6 py-4 text-lg font-bold text-zinc-50"
+    class="absolute bottom-15 left-0 line-clamp-2 w-86 bg-red-900/85 px-6 py-4 text-xl font-bold text-zinc-50"
   >
     Theater<br />
     Verlängertes Wohnzimmer
@@ -31,17 +31,17 @@
 
 <h2 class="mx-6 mb-4 text-xl font-bold text-neutral-700">Unsere nächste Veranstaltung</h2>
 
-<div class="mx-6 mb-4 text-base text-neutral-700">
+<div class="mx-6 mb-1 text-base text-neutral-700">
   {formatStartDate(nextEvent.start_date, nextEvent.start_time)}
 </div>
 
-<h1 class="mx-6 mb-4 text-2xl font-bold text-neutral-700">
+<h1 class="mx-6 mb-2 flex flex-row gap-x-2 text-2xl font-bold text-neutral-700">
   {formatStartTime(nextEvent.start_date, nextEvent.start_time)}
 
   <a href="/productions/{nextProduction.id}">{nextProduction.title_de}</a>
 </h1>
 
-<div class="mx-6 mb-6 flex flex-row space-x-2 text-sm">
+<div class="mx-6 mb-2 flex flex-row space-x-2 text-sm">
   {#each nextEvent.characteristics as characteristic}
     {@render characteristicChip(characteristic)}
   {/each}
@@ -51,8 +51,8 @@
   {/each}
 </div>
 
-<div class="mx-6 mb-8 line-clamp-3 text-base whitespace-pre-line">
-  {nextProduction.description_de}
+<div class="mx-6 mb-2 line-clamp-3 text-base whitespace-pre-line">
+  {nextProduction.subtitle_de}
 </div>
 
 <div class="mx-6 mb-8 flex flex-row items-center">
@@ -73,24 +73,24 @@
   >Spielplan</a
 >
 
-<h2 class="mx-6 mb-4 text-2xl">Produktionen im Fokus</h2>
+<h2 class="mx-6 mb-4 border-b-1 border-neutral-700 text-2xl">Produktionen im Fokus</h2>
 
 {#each data.productions as production}
-  <div class="relative mb-8 h-50 w-full bg-slate-200">
+  <div class="relative mx-6 mb-4 h-50 bg-slate-200">
     {@render categoryCaption(production.categories[0])}
 
     {@render mainImage(production.images?.[0])}
   </div>
 
-  <h2 class="mx-6 mb-2 text-xl">
+  <h2 class="mx-6 mb-2 text-2xl font-bold text-neutral-700">
     <a href="/productions/{production.id}">{production.title_de}</a>
   </h2>
 
-  <div class="mx-6 mb-8 line-clamp-3 whitespace-pre-line">{production.description_de}</div>
+  <div class="mx-6 mb-6 line-clamp-6 whitespace-pre-line">{production.description_de}</div>
 
   <a
     href="/productions/{production.id}"
-    class="mx-6 mb-6 block rounded-sm bg-red-900 px-4 py-2 text-center text-2xl text-zinc-50"
+    class="mx-6 mb-6 block rounded-sm bg-red-900 px-4 py-2 text-center text-xl text-zinc-50"
     >Infos & Termine</a
   >
 {/each}
